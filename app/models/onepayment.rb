8f448e2355956cc9878f4b61ed4d7e775e01fbf0
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2021-01-17 17:20:50 +0900

    バリデーション追加

--- app/models/onepayment.rb
+++ app/models/onepayment.rb
@@ -8,9 +8,9 @@ class Onepayment < ApplicationRecord
 
   with_options presence: true do
     validates :month_id,:day_id,:store_id,:kisyu_id, numericality: { other_than: 1 }
-    validates :one_investment
-    validates :one_recovery
-    validates :one_balance_of_payment
+    validates :one_investment, format: { with: /\A[0-9]+\z/ , message: "with a half size number"}
+    validates :one_recovery, format: { with: /\A[0-9]+\z/ , message: "with a half size number"}
+    validates :one_balance_of_payment, format: { with: /\A[0-9]+\z/ , message: "with a half size number"}
     validates :one_rationale
   end
 
