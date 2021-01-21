2356829dea7a51298052e510ead0d461a35e1667
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2021-01-22 03:59:27 +0900

    グラフ表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     user=User.find(params[:id])
     @onepayments = user.onepayments.order("id DESC")
+    @data = [['2019-06-01', 100], ['2019-06-02', 500], ['2019-06-03', 150]]
   end
 
 end
