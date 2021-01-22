d87a9dac29a6b53d161d1bd5b7b6e692afc83c02
mahesvara5829 <76080506+mahesvara5829@users.noreply.github.com>
2021-01-22 18:32:10 +0900

    レスポンシブデザイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,6 @@ class UsersController < ApplicationController
   def show
     user=User.find(params[:id])
     @onepayments = user.onepayments.order("id DESC")
-    @sum=0
-    @onepayments.each do |onepayment|
-      @sum=@sum+onepayment.one_balance_of_payment.to_i
-    end
-
   end
 
 end
